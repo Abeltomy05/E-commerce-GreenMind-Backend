0d366f535cab138319d58488b841a9823448f30d
Abel Tomy <abeltomy3@gmail.com>
2025-02-01 17:28:33 +0530

    host

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ passport.deserializeUser((user, done)=>done(null, user));
 
 
 const corsOptions = {
-    origin: 'https://abeltomy.site', 
+    origin: 'https://www.abeltomy.site', 
     credentials: true, 
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
